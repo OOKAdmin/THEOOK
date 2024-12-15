492b55b4ea0f23c2f87deb67961adb51c38c1e7a
OOKAdmin <ookdata111@gmail.com>
2024-12-15 20:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -122,4 +122,5 @@ def handle_data():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
